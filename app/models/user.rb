a87ab91fb81c1147c3876a93dba80b958f13b07e
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 15:53:56 -0600

    Added allergies list to user profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :allergies
     has_many :ingredients, through: :allergies
+
+    def allergy_ingredients
+        self.allergies.map { |allergy| allergy.ingredient.name }
+    end
+
 end
