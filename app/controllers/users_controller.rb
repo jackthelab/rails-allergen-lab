d191ef378c0cc4ce4c99ba9a1cab4100c0201b3b
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 14:43:15 -0600

    Added delete option for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
 
     def show
+        find_user
+    end
+
+    def destroy
+        find_user.destroy
+    end
+
+    private
+    
+    def find_user
+        @user = User.find_by(id: params[:id])
     end
     
 end
