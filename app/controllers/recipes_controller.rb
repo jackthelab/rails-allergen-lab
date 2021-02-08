ba2d6d71e4bf3d26f8e2f79187bb16ef8a6fbb2d
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 16:58:25 -0600

    Added the ability to build recipes with multiple ingredients and add a user. Additon instances are created too if the recipe can successfully be saved.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,12 +6,23 @@ class RecipesController < ApplicationController
     end
 
     def new
+        @recipe = Recipe.new
     end
 
     def recipe_params
+        params.require(:recipe).permit(:name, :user_id)
     end
 
     def create
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.save
+            params[:recipe][:ingredients].each { |ingredient| Addition.create(recipe_id: @recipe.id, ingredient_id: ingredient) }
+            redirect_to recipes_path
+        else
+            redirect_to new_recipe_path
+        end
+
     end
     
 end
