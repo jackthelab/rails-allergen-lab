f404f002911b0ce17b53911570ed0e210cfbe656
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 12:10:51 -0600

    Added controller files and view folders
    eiaodpfeofjhewoighoeioej

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :ingredients, only: [:index, :show, :new, :create]
+  resources :recipes, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create, :destroy]
 end
