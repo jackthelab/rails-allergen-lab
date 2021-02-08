41bb89a86f1541e24f197cf55bb34615ff048a0b
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 12:14:58 -0600

    Updated controllers with new actions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,17 @@
 class IngredientsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def ingredients_params
+  end
+
+  def create
+  end
+  
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,17 @@
 class RecipesController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def recipe_params
+    end
+
+    def create
+    end
+    
 end
