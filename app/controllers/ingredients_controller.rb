77084cbe565f83bbf3352a8e8c0c0240430a007e
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 17:46:16 -0600

    Ordered index page for ingredients by allergy count

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,6 +2,7 @@ class IngredientsController < ApplicationController
 
   def index
     @ingredients = Ingredient.all
+    @ingredients_by_allergies = Ingredient.ordered_by_users_allergic
   end
 
   def show
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,12 @@ class Ingredient < ApplicationRecord
     has_many :recipes, through: :additions
     has_many :allergies
     has_many :users, through: :allergies
+
+    def allergy_count
+        Allergy.where(ingredient_id: self.id).count
+    end
+
+    def self.ordered_by_users_allergic
+        Ingredient.all.sort_by { |ingredient| ingredient.allergy_count }.reverse
+    end
 end
