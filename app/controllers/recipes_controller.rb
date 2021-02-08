c93af234a5b11b50642683089893c07e879bb005
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 16:08:55 -0600

    made Recipes index show up in order of number of ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
 
     def index
         @recipes = Recipe.all
+        @recipes_no_of_ingredients_order = Recipe.sorted_by_number_of_ingredients
     end
 
     def new
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,12 @@ class Recipe < ApplicationRecord
     belongs_to :user
     has_many :additions
     has_many :ingredients, through: :additions
+
+    def num_of_ingredients
+        self.ingredients.count
+    end
+
+    def self.sorted_by_number_of_ingredients
+        Recipe.all.sort_by { |recipe| recipe.num_of_ingredients }.reverse
+    end
 end
